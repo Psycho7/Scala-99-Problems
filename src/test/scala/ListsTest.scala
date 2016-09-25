83db51950b5447413cee2b7f463efe9b3a08ec48
Chujie Zeng <chujie.zeng@gmail.com>
2016-09-26 02:51:10 +0800

    Revised Test Case

--- src/test/scala/ListsTest.scala
+++ src/test/scala/ListsTest.scala
@@ -6,13 +6,32 @@ import org.scalatest._
 class ListsTest extends FlatSpec {
   import Lists._
 
-  "A list" should "return its last element" in {
+  "last method" should "return its last element" in {
     val list = List(1, 1, 2, 3, 5, 8)
     assert(P01.last(list) == 8)
+    val list2 = List(5)
+    assert(P01.last(list2) == 5)
   }
 
-  it should "return its last but one element" in {
+  it should "throw NoSuchElementException if a list has less than 1 element" in {
+    assertThrows[NoSuchElementException] {
+      P01.last(List())
+    }
+  }
+
+  "penultimate method" should "return its last but one element" in {
     val list = List(1, 1, 2, 3, 5, 8)
     assert(P02.penultimate(list) == 5)
+    val list2 = List(1, 2)
+    assert(P02.penultimate(list2) == 1)
+  }
+
+  it should "throw NoSuchElementException if a list has less than 2 elements" in {
+    assertThrows[NoSuchElementException] {
+      P02.penultimate(List())
+    }
+    assertThrows[NoSuchElementException] {
+      P02.penultimate(List(1))
+    }
   }
 }
