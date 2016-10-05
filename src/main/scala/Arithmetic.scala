4fd4ee99150c293236e42a9016dc14f6ce9e965f
Chujie Zeng <chujie.zeng@gmail.com>
2016-10-05 15:02:12 +0800

    P31 - P33

--- src/main/scala/Arithmetic.scala
+++ src/main/scala/Arithmetic.scala
@@ -4,9 +4,26 @@
 object Arithmetic {
   class S99Int(val start: Int) {
     import S99Int._
+
+    // P31
+    def isPrime: Boolean =
+      (start > 1) && (2 to Math.sqrt(start).toInt).forall{ start % _ != 0 }
+
+    // P33
+    def isCoprimeTo(that: S99Int): Boolean = gcd(this, that) == 1
   }
 
   object S99Int {
     implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
+
+    implicit def s99toInt(i: S99Int): Int = i.start
+
+    // P32
+    def gcd(x: Int, y: Int): Int = {
+      val r = x % y
+      if (r == 0) y
+      else gcd(y, r)
+    }
   }
+
 }
--- src/test/scala/ArithmeticTest.scala
+++ src/test/scala/ArithmeticTest.scala
@@ -1,8 +1,31 @@
 /**
   * Created by Psycho7 on 10/5/16.
   */
+import Arithmetic.S99Int
 import org.scalatest._
 
 class ArithmeticTest extends FlatSpec {
+  import S99Int._
 
+  "isPrime method" should "determine whether a integer is prime" in {
+    assert(2.isPrime)
+    assert(3.isPrime)
+    assert(!4.isPrime)
+  }
+
+  "gcd method" should "determine the greatest common divisor of two positive integer numbers" in {
+    assert(gcd(2, 4) == 2)
+    assert(gcd(22, 121) == 11)
+    assert(gcd(121, 22) == 11)
+    assert(gcd(3, 5) == 1)
+    assert(gcd(5, 3) == 1)
+  }
+
+  "isCoprimeTo method" should "determine whether two positive integer numbers are coprime" in {
+    assert(2.isCoprimeTo(3))
+    assert(2.isCoprimeTo(5))
+    assert(5.isCoprimeTo(4))
+    assert(!2.isCoprimeTo(4))
+    assert(!121.isCoprimeTo(33))
+  }
 }
