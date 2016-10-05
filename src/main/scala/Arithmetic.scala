0e2e2df4db55638c21ae9235718a426dc8f7747b
Chujie Zeng <chujie.zeng@gmail.com>
2016-10-05 13:55:15 +0800

    Arithmetic Init

---
+++ src/main/scala/Arithmetic.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by Psycho7 on 10/5/16.
+  */
+object Arithmetic {
+  class S99Int(val start: Int) {
+    import S99Int._
+  }
+
+  object S99Int {
+    implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
+  }
+}
---
+++ src/test/scala/ArithmeticTest.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Psycho7 on 10/5/16.
+  */
+import org.scalatest._
+
+class ArithmeticTest extends FlatSpec {
+
+}
