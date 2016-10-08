f366f092b807b68f88a04a3f5d33234e29b86ac1
Chujie Zeng <chujie.zeng@gmail.com>
2016-10-08 18:29:44 +0800

    Logic Init

---
+++ src/main/scala/Logic.scala
@@ -0,0 +1,7 @@
+/**
+  * Created by Psycho7 on 10/8/16.
+  */
+object Logic {
+  object S99Logic {
+  }
+}
---
+++ src/test/scala/LogicTest.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by Psycho7 on 10/8/16.
+  */
+import org.scalatest._
+
+class LogicTest extends FlatSpec {
+  import Logic._
+
+}
