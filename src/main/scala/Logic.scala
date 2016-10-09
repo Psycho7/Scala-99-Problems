2fa1cd7f082ad01609e98338fb60438cf3a12559
Chujie Zeng <chujie.zeng@gmail.com>
2016-10-09 10:43:54 +0800

    Basic Operation

--- src/main/scala/Logic.scala
+++ src/main/scala/Logic.scala
@@ -3,5 +3,37 @@
   */
 object Logic {
   object S99Logic {
+    def not(a: Boolean) = a match {
+      case true => false
+      case false => true
+    }
+
+    def and(a: Boolean, b: Boolean) = (a, b) match {
+      case (true, true) => true
+      case _ => false
+    }
+
+    def or(a: Boolean, b: Boolean) = (a, b) match {
+      case (false, false) => false
+      case _ => true
+    }
+
+    def equ(a: Boolean, b: Boolean) = (a, b) match {
+      case (true, true) => true
+      case (false, false) => true
+      case _ => false
+    }
+
+    def nand(a: Boolean, b: Boolean) = not(and(a, b))
+
+    def nor(a: Boolean, b: Boolean) = not(or(a, b))
+
+    def xor(a: Boolean, b: Boolean) = (a, b) match {
+      case (true, false) => true
+      case (false, true) => true
+      case _ => false
+    }
+
+    def impl(a: Boolean, b: Boolean) = or(not(a), b)
   }
 }
