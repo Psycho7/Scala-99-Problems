edb1154f2c314a201d794b4af3f68db2fc07d34c
Chujie Zeng <chujie.zeng@gmail.com>
2016-10-11 12:15:04 +0800

    P50

--- src/main/scala/Logic.scala
+++ src/main/scala/Logic.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 /**
   * Created by Psycho7 on 10/8/16.
   */
@@ -24,12 +26,53 @@ object Logic {
       }
     }
 
+    // P49
     def gray(n: Int): List[String] =
       if (n == 0) List("")
       else {
         val pre = gray(n - 1)
         pre.map{ "0" + _ } ::: pre.reverse.map{ "1" + _ }
       }
+
+    // P50
+    def huffman(ls: List[(String, Int)]): List[(String, String)] = {
+      type DString = (String, String)
+
+      abstract class Tree {
+        val freq: Int
+        def toCode: List[DString]
+        def toCodePrefixed(prefix: String) = toCode map {
+          case (sym, code) => (sym, prefix ++ code)
+        }
+      }
+
+      final case class Fork(left: Tree, right: Tree) extends Tree {
+        val freq = left.freq + right.freq
+        def toCode: List[DString] = left.toCodePrefixed("0") ++ right.toCodePrefixed("1")
+      }
+
+      final case class Node(sym: String, freq: Int) extends Tree {
+        def toCode: List[DString] = List((sym, ""))
+      }
+
+      object TreeOrdering extends Ordering[Tree] {
+        def compare(a: Tree, b: Tree) = b.freq compare a.freq
+      }
+
+      import scala.collection.mutable
+      def loop(queue: mutable.PriorityQueue[Tree]): Tree = {
+        if (queue.length == 1) queue.dequeue
+        else {
+          val a, b = queue.dequeue
+          //val b = queue.dequeue
+          queue.enqueue(Fork(a, b))
+          loop(queue)
+        }
+      }
+
+      val queue = mutable.PriorityQueue[Tree](ls map { case (sym, freq) => Node(sym, freq) }: _*)(TreeOrdering)
+      loop(queue).toCode.sortBy{ case (sym, _) => sym }
+    }
   }
 
   // P47
--- src/test/scala/LogicTest.scala
+++ src/test/scala/LogicTest.scala
@@ -10,4 +10,11 @@ class LogicTest extends FlatSpec {
     assert(gray(2) == List("00", "01", "11", "10"))
     assert(gray(3) == List("000", "001", "011", "010", "110", "111", "101", "100"))
   }
+
+  "huffman method" should "create a Huffman Code list" in {
+    val ls = List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5))
+    assert(huffman(ls) == List(("a", "0"), ("b", "101"),
+      ("c", "100"), ("d", "111"),
+      ("e", "1101"), ("f", "1100")))
+  }
 }
