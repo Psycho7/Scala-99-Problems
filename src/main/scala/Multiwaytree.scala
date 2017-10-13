7dc43f1137a47c61290bcca497f92ca6e76796a9
Chuck Zeng <chujie.zeng@gmail.com>
2017-10-13 14:20:25 -0400

    P70C

--- src/main/scala/Multiwaytree.scala
+++ src/main/scala/Multiwaytree.scala
@@ -3,6 +3,10 @@ object Multiwaytree {
   case class MTree[+T](value: T, children: List[MTree[T]]) {
     def this(value: T) = this(value, List())
     override def toString: String = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+    def nodeCount: Int = children match {
+      case List() => 1
+      case _ => children.map(_.nodeCount).sum + 1
+    }
   }
 
   object MTree {
--- src/test/scala/MultiwayTreeTest.scala
+++ src/test/scala/MultiwayTreeTest.scala
@@ -2,4 +2,10 @@ import org.scalatest._
 
 class MultiwayTreeTest extends FlatSpec {
   import Multiwaytree._
+
+  // P70C
+  "nodeCount" should "count the nodes of a multiway tree" in {
+    assert(MTree('a').nodeCount == 1)
+    assert(MTree('a', List(MTree('f'))).nodeCount == 2)
+  }
 }
