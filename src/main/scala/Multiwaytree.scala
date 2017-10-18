72bb22720fee0d1ecaadcc5069ca24fddaef3424
Chujie Zeng <chujie.zeng@gmail.com>
2017-10-18 18:08:57 -0400

    P70

--- src/main/scala/Multiwaytree.scala
+++ src/main/scala/Multiwaytree.scala
@@ -2,7 +2,7 @@ object Multiwaytree {
 
   case class MTree[+T](value: T, children: List[MTree[T]]) {
     def this(value: T) = this(value, List())
-    override def toString: String = "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
+    override def toString: String = value.toString + children.map(_.toString).mkString("") + "^"
     def nodeCount: Int = children match {
       case List() => 1
       case _ => children.map(_.nodeCount).sum + 1
@@ -11,6 +11,18 @@ object Multiwaytree {
 
   object MTree {
     def apply[T](value: T) = new MTree(value, List())
-    // def apply[T](value: T, children: List[MTree[T]]) = new MTree(value, children)
+
+    implicit def str2MTree(str: String): MTree[Char] = {
+      def boundary(pos: Int, sum: Int): Int =
+        if (sum == 0) pos
+        else boundary(pos + 1, if (str(pos) == '^') sum - 1 else sum + 1)
+      def childStrings(pos: Int): List[String] =
+        if (pos + 1 >= str.length) Nil
+        else {
+          val bound = boundary(pos + 1, 1)
+          str.substring(pos, bound - 1) :: childStrings(bound)
+        }
+      MTree(str(0), childStrings(1).map(str2MTree _))
+    }
   }
 }
--- src/test/scala/MultiwayTreeTest.scala
+++ src/test/scala/MultiwayTreeTest.scala
@@ -8,4 +8,13 @@ class MultiwayTreeTest extends FlatSpec {
     assert(MTree('a').nodeCount == 1)
     assert(MTree('a', List(MTree('f'))).nodeCount == 2)
   }
+
+  // P70
+  "str2MTree" should "convert string to MTree" in {
+    val tree = MTree('a',
+      List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e')))))
+    val str = "afg^^c^bd^e^^^"
+    assert(tree.toString == str)
+    assert(MTree.str2MTree(str).toString == str)
+  }
 }
