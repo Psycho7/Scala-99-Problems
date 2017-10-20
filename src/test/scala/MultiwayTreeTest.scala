45f7b076cad27cb8b98ee215eaf167a50272140f
Chujie Zeng <chujie.zeng@gmail.com>
2017-10-20 13:39:05 -0400

    P73

--- src/test/scala/MultiwayTreeTest.scala
+++ src/test/scala/MultiwayTreeTest.scala
@@ -2,7 +2,7 @@ import org.scalatest._
 
 class MultiwayTreeTest extends FlatSpec {
   import Multiwaytree.MTree
-  import Multiwaytree.MTree.str2MTree
+  import Multiwaytree.MTree._
 
   // P70C
   "nodeCount" should "count the nodes of a multiway tree" in {
@@ -31,5 +31,22 @@ class MultiwayTreeTest extends FlatSpec {
     assert("a^".postorder == List('a'))
     assert("ab^c^d^^".postorder == 'b' :: 'c' :: 'd' :: 'a' :: Nil)
     assert("afg^^c^bd^e^^^".postorder == List('g', 'f', 'c', 'd', 'e', 'b', 'a'))
+  }
+
+  // P73
+  "lispyTree" should "construct the listy representation of a multiway tree" in {
+    assert("a^".lispyTree == "a")
+    assert("ab^^".lispyTree == "(a b)")
+    assert("abc^^^".lispyTree == "(a (b c))")
+    assert("bd^e^^".lispyTree == "(b d e)")
+    assert("afg^^c^bd^e^^^".lispyTree == "(a (f g) c (b d e))")
+  }
+
+  "fromLispy" should "construct MTree from lispy string" in {
+    assert(fromLispy("a").lispyTree == "a")
+    assert(fromLispy("(a b)").lispyTree == "(a b)")
+    assert(fromLispy("(a (b c))").lispyTree == "(a (b c))")
+    assert(fromLispy("(b d e)").lispyTree == "(b d e)")
+    assert(fromLispy("(a (f g) c (b d e))").lispyTree == "(a (f g) c (b d e))")
   }
 }
